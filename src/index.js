d38399cb9a0afa90c8e15971aaea6865c47d03ac
francisco <franciscobarrera968@gmail.com>
2022-04-19 18:52:30 -0300

    Configuracion de CORS parte 5

--- src/index.js
+++ src/index.js
@@ -16,7 +16,14 @@ app.use(morgan("dev"));
 
 
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'x-access-token, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 
+  next();
+});
 
 
 
