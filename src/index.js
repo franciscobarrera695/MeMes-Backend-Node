740dce38c323bc3e5b60258298b49740a4a936f0
francisco <franciscobarrera968@gmail.com>
2022-04-19 18:39:36 -0300

    Configuracion de CORS parte 2

--- src/index.js
+++ src/index.js
@@ -14,6 +14,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'x-access-token, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+  next();
+});
+
+
+
 const dominiosPermitidos = [process.env.REACT_FRONTEND_URL]
 const corsOption = {
   origin:function(origin,callback){
