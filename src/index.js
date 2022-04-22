fdf940a1262e04966a41942326bf9092bd3c845b
francisco <franciscobarrera968@gmail.com>
2022-04-22 19:09:13 -0300

    Arreglo del codigo parte 3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import app from "./app"
-import "./database.js";
-import {PORT} from "./config/dotenv"
+import app from "./app.js"
+import {connectDB} from "./database.js";
+import {PORT} from "./config/dotenv.js"
 connectDB()
 app.listen(process.env.PORT || 5000, () => {
   console.log("server listening");
